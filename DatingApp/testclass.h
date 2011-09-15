1feee3eb0335aabc4a8ce2a20ab3d89651ddd3fc
Lee Atkinson <leeroy.atkinson@gmail.com>
2011-09-15 15:52:14 +0100

    new files

---
+++ DatingApp/testclass.h
@@ -0,0 +1,13 @@
+//
+//  testclass.h
+//  DatingApp
+//
+//  Created by lee atkinson on 15/09/2011.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface testclass : NSObject
+
+@end
---
+++ DatingApp/testclass.m
@@ -0,0 +1,13 @@
+//
+//  testclass.m
+//  DatingApp
+//
+//  Created by lee atkinson on 15/09/2011.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "testclass.h"
+
+@implementation testclass
+
+@end
